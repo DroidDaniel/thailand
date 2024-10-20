2a047c4ce855b3b0a1fdcfee6391663423415d5b
Sri <sriar.srivati@ansira.com>
2024-10-20 11:34:24 -0700

    cost detail

--- js/script.js
+++ js/script.js
@@ -4,4 +4,16 @@ $(document).ready(function () {
     const link = $(this).data("link");
     window.location.href = link;
   });
+  const correctPin = "1234"; // Change this to your desired PIN
+
+  function validatePin() {
+    const userPin = document.getElementById("pinInput").value;
+
+    if (userPin === correctPin) {
+      document.getElementById("itinerary").style.display = "block"; // Show the itinerary section
+      document.getElementById("loginContainer").style.display = "none"; // Hide the login form
+    } else {
+      alert("Incorrect PIN. Please try again.");
+    }
+  }
 });
